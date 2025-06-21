e1de5477f5e018eebfa3f336acdce7200a59cb36
Shaurya Gupta <vishnu8858369983@gmail.com>
2025-06-21 20:12:44 +0530

    Project initialisation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='flex  gap-6 items-center justify-center min-h-screen'> 
+      <h1>Hello world</h1>
+      <button className='btn'>submit</button>
+    </div>
+    </>
+  )
+}
+
+export default App
